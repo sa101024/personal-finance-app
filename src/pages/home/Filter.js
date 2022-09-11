97ea920af23ae5816e0a6264df8f21b94ec14373
Satoko Oiwa <satoko.oiwa@gmail.com>
2022-09-11 16:55:03 +0900

    Add Filter

---
+++ src/pages/home/Filter.js
@@ -0,0 +1,24 @@
+const filterList = ['All', '支出', '収入', '光熱費', '食費雑費', '本、映画', 'その他-収入', 'その他-支出']
+
+export default function Filter({ currentFilter, changeFilter }) {
+  const handleClick = (newFilter) => {
+    changeFilter(newFilter)
+  }
+
+  return (
+    <div className="filter">
+      <nav>
+        <p>Filter by:</p>
+        {filterList.map(f => (
+          <button
+            key={f}
+            className={currentFilter === f ? 'active' : ''}
+            onClick={() => handleClick(f)}
+          >
+            {f}
+          </button>
+        ))}
+      </nav>
+    </div>
+  )
+}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,17 @@
-import { useCollection } from '../../hooks/useCollection'
+import { useState } from 'react' 
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection'
 
 // components
 import ExpensesList from './ExpensesList';
 import CreateForm from './CreateForm';
+import Filter from './Filter';
 
 // styles
 import './Home.css'
 
 export default function Home() {
+  const [currentFilter, setCurrentFilter] = useState('All')
   const { user } = useAuthContext()
   const { documents, error } = useCollection(
     'expenses',
@@ -16,15 +19,50 @@ export default function Home() {
     ['date', 'asc']
   )
 
-    return (
+  const changeFilter = (newFilter) => {
+   setCurrentFilter(newFilter)
+  }
+
+  const expenses = documents ? documents.filter((document) => {
+    switch (currentFilter) {
+      case 'All':
+        console.log(currentFilter)
+        return true;
+      case '支出':
+      case '収入':
+        console.log(currentFilter)
+        return document.category === currentFilter;
+      case '光熱費':
+      case '食費雑費':
+      case '本、映画':
+      case 'その他-収入':
+      case 'その他-支出':
+        console.log(currentFilter)
+        return document.accountTitle === currentFilter;
+      default:
+        return true;
+    }
+  }) : null
+
+  return (
     <div className='home-container'>
-      <div className='content'>
+      <div className='home-filter'>
+        {documents && (
+          <Filter
+            currentFilter={currentFilter}
+            changeFilter={changeFilter}
+          />
+        )}
+      </div>
+      <div className='home-content'>
+        <div className='list'>
         <h2 className='page-title'>Expense list</h2>
         {error && <p className='error'>{error}</p>}
-        {documents && <ExpensesList expenses={documents} />}
-      </div>
-      <div className='create-form'>
+        {expenses && <ExpensesList expenses={expenses} />}
+        </div>
+      <div className='create'>
         <CreateForm uid={user.uid}/>
+      </div>
       </div>
     </div>
   )
