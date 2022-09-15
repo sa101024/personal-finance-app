2364b469050027fc6f33079c2bec79973fc7661a
Satoko Oiwa <satoko.oiwa@gmail.com>
2022-09-16 00:44:53 +0900

    Add total calculation

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react' 
+import { useEffect, useState } from 'react' 
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { useCollection } from '../../hooks/useCollection'
 
@@ -9,14 +9,16 @@ import Filter from './Filter';
 
 // styles
 import './Home.css'
+// import ExpensesTotal from './ExpensesTotal';
 
 export default function Home() {
   const [currentFilter, setCurrentFilter] = useState('All')
+  const [expensesTotal, setExpensesTotal] = useState(0)
   const { user } = useAuthContext()
   const { documents, error } = useCollection(
     'expenses',
     ['uid', '==', user.uid],
-    ['date', 'asc']
+    ['date', 'desc']
   )
 
   const changeFilter = (newFilter) => {
@@ -26,23 +28,33 @@ export default function Home() {
   const expenses = documents ? documents.filter((document) => {
     switch (currentFilter) {
       case 'All':
-        console.log(currentFilter)
         return true;
       case '支出':
       case '収入':
-        console.log(currentFilter)
         return document.category === currentFilter;
       case '光熱費':
       case '食費雑費':
       case '本、映画':
-      case 'その他-収入':
-      case 'その他-支出':
-        console.log(currentFilter)
+      case 'その他収入':
+      case 'その他支出':
         return document.accountTitle === currentFilter;
       default:
         return true;
     }
   }) : null
+
+  useEffect(() => {
+    const calcTotal = async() => {
+      let sum = 0
+      const expenseAmountList = await expenses ? expenses.map((expense) => expense.amount.split(',')) : null
+      expenseAmountList.forEach(expenseAmount => {
+        let number = Number(expenseAmount.join(''))
+          sum += number
+      })
+      setExpensesTotal(Number(sum).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','))
+    }
+    calcTotal()
+  }, [expenses])
 
   return (
     <div className='home-container'>
@@ -53,6 +65,10 @@ export default function Home() {
             changeFilter={changeFilter}
           />
         )}
+        <div className='home-total'>
+          <p>Total: </p>
+          <span>{expensesTotal}</span>
+        </div>
       </div>
       <div className='home-content'>
         <div className='list'>
